78590f17358d1dbd86095d453854af3677528ab0
john8707 <doobgyul@gmail.com>
2020-04-10 20:16:43 +0900

    ADD(model) : create custom user model

--- banhyang/core/admin.py
+++ banhyang/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'username',
+        'email',
+        'phonenumber',
+        'gisu',
+        'date_joined'
+    )
+
+    list_display_links = (
+        'username',
+        'email'
+    )
