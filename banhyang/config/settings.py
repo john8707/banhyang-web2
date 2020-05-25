fbba00b6286996c4143b181456d1ee61f652a239
john8707 <doobgyul@gmail.com>
2020-05-26 04:29:58 +0900

    collect statics

--- banhyang/config/settings.py
+++ banhyang/config/settings.py
@@ -14,6 +14,7 @@ import os, json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -138,11 +139,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    'core/static',
+    'banhyang/core/static',
     
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 AUTH_USER_MODEL = 'core.User'
