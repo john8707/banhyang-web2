4e069a734d2481df62e6459fe48cda0b9371d380
john8707 <doobgyul@gmail.com>
2020-05-26 05:43:40 +0900

    DELETE custom user model

--- banhyang/core/admin.py
+++ banhyang/core/admin.py
@@ -2,18 +2,3 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-@admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
-
-    list_display = (
-        'username',
-        'email',
-        'phonenumber',
-        'gisu',
-        'date_joined'
-    )
-
-    list_display_links = (
-        'username',
-        'email'
-    )
