36a304915176f7ef07e39e5d5ef1f8c9db1189e6
john8707 <doobgyul@gmail.com>
2023-09-26 18:10:53 +0900

    UPDATE DB 엔진 for foreign key constraints

--- banhyang/custom_db_backends/vitess/base.py
+++ banhyang/custom_db_backends/vitess/base.py
@@ -1,9 +1,7 @@
 from django.db.backends.mysql.base import DatabaseWrapper as MysqlDatabaseWrapper
 from .features import DatabaseFeatures
 
-class DatabaseWrapper(MysqlDatabaseWrapper):
-    vendor = 'vitess'
 
-    def __init__(self, *args, **kwargs):
-        super(DatabaseWrapper, self).__init__(*args, **kwargs)
-        self.features = DatabaseFeatures(self)
+class DatabaseWrapper(MysqlDatabaseWrapper):
+    vendor = 'planetscale'
+    features_class = DatabaseFeatures
