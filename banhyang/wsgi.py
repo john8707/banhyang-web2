1742c9de1b786f32c83fbd220e94587a195c61c1
john8707 <doobgyul@gmail.com>
2023-09-28 22:15:06 +0900

    FEAT get or create superuser when starting server
    서버 재시작시 슈퍼유저 계정 존재하는지 확인 후 없을 경우 생성

--- banhyang/wsgi.py
+++ banhyang/wsgi.py
@@ -16,3 +16,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banhyang.config.settings')
 application = get_wsgi_application()
 from django.core.management import call_command
 call_command('migrate')
+
+from django.contrib.auth import get_user_model
+User = get_user_model()  # get the currently active user model,
+if os.getenv("DJANGO_SUPERUSER_USERNAME", None) and os.getenv("DJANGO_SUPERUSER_PASSWORD", None):
+    User.objects.filter(username= os.getenv("DJANGO_SUPERUSER_USERNAME")).exists() or \
+        call_command('createsuperuser', interactive=False)
