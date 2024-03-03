b8b79d90f6729bc1fd62215d1c70f476bcf448a4
john8707 <doobgyul@gmail.com>
2024-03-04 03:11:07 +0900

    UPDATE URL Structure 변경

--- banhyang/practice/urls.py
+++ banhyang/practice/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('practice/create', views.create, name='create'),
     path('practice/songs', views.song_list, name='song_list'),
     path('practice/users', views.user_list, name='user_list'),
-    path('practice/schedule/create', views.schedule_create, name='schedule_create'),
-    path('practice/schedule/<int:schedule_id>', views.practice_delete, name='practice_delete'),
-    path('practice/schedule/NA', views.who_is_not_coming, name='who_is_not_coming')
+    path('practice/timetable', views.timetable, name='timetable'),
+    path('practice/delete/<int:schedule_id>', views.practice_delete, name='practice_delete'),
+    path('practice/NA', views.who_is_not_coming, name='who_is_not_coming')
 ]
