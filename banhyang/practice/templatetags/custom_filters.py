5d15f454771425738ddb92ef37cbeb14c72a8074
john8707 <doobgyul@gmail.com>
2024-03-11 22:40:09 +0900

    UPDATE 곡 목록 페이지 데스크탑, 모바일 가시성 개선

--- banhyang/practice/templatetags/custom_filters.py
+++ banhyang/practice/templatetags/custom_filters.py
@@ -9,3 +9,13 @@ def get_from_key(dict, key):
         return ""
     else:
         return "X"
+
+
+@register.filter('get_from_key_mobile')
+def get_from_key_mobile(dict, key):
+    if key in dict:
+        return dict[key].replace(',', "<br>")
+    elif key == "etc":
+        return ""
+    else:
+        return "X"
