841988c7bbe75f41bb1012a04d57b7d7c082689b
john8707 <doobgyul@gmail.com>
2024-04-18 15:17:13 +0900

    !FIX! Chatbot timezone 오류
    챗봇 출석체크 시도시, 당일 합주 존재 여부 가져오는 과정에서 Date Field에 Timezone으로 Filter시 Oracle DB에서 오류 발생

--- banhyang/practice/forms.py
+++ banhyang/practice/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Schedule, PracticeUser, Timetable, AttendanceCheck, Apply, WhyNotComing
+from .models import Schedule, PracticeUser, Apply, WhyNotComing
 from django.core.exceptions import ValidationError
 import datetime
 from datetime import date
@@ -210,55 +210,3 @@ class UserAddForm(forms.Form):
                 raise ValidationError("해당 인원이 이미 존재합니다. 동명이인의 경우 숫자, 세션등을 이용해 구분하여 주세요.")
         except PracticeUser.DoesNotExist:
             return form_data
-        
-
-class AttendanceCheckForm(forms.Form):
-    username = forms.CharField(required=True)
-
-    def clean(self):
-        form_data = self.cleaned_data
-
-        now = datetime.datetime.now()
-
-        #input한 이름의 유저가 존재하는지 validate
-        try:
-            user_exist = PracticeUser.objects.get(username=form_data['username'])
-        except PracticeUser.DoesNotExist:
-            raise ValidationError("해당 인원이 존재하지 않습니다. 오탈자를 다시 확인하거나 관리자에게 문의하세요.")
-        
-        #오늘 날짜 date + 0시0분0초 -> datetime 형식으로
-        arrived_day_datetime = datetime.datetime.combine(datetime.date.today(), datetime.time(0,0,0,0,datetime.timezone.utc))
-
-        #오늘 합주가 존재하는지 validate
-        schedule_exist = Schedule.objects.filter(date=arrived_day_datetime)
-        if not schedule_exist:
-            raise ValidationError("금일 예정된 합주가 없습니다. 날짜를 다시 확인하거나 관리자에게 문의하세요.")
-        
-        #저장된 합주 시간표가 있는지 validate
-        timetable_exist = Timetable.objects.filter(schedule_id__in=schedule_exist)
-        if not timetable_exist:
-            raise ValidationError("데이터베이스에 저장된 시간표가 존재하지 않습니다. 관리자에게 문의하세요.")
-
-        #현재 인증가능한 합주가 있는지
-        song_now = timetable_exist.filter(start_time__lte = (now + datetime.timedelta(minutes=4)).time(), end_time__gt=(now + datetime.timedelta(minutes=4)).time())
-        if not song_now:
-            raise ValidationError("현재 출석 체크 가능한 곡이 존재하지 않습니다. 합주 시작 4분전 ~ 종료 4분전까지 인증 가능합니다.")
-        
-
-
-        #현재 진행중인 곡이 내가 참여하는 곡인지
-        do_i_play_list=[]
-        for i in song_now:
-            do_i_play = i.song_id.session.filter(user_name=user_exist)
-            if do_i_play:
-                do_i_play_list.append(i)
-        
-        if not do_i_play_list:
-            raise ValidationError("현재 출석 체크 가능한 곡 중 연주하는 곡이 없습니다. 합주 시작 4분전 ~ 종료 4분전까지 인증 가능합니다.")
-        
-        attendance_exist = AttendanceCheck.objects.filter(timetable_id__in=do_i_play_list, user_name=user_exist)
-        if attendance_exist:
-            raise ValidationError("이미 인증이 완료되었습니다. 합주 시작 4분전 ~ 종료 4분전까지 인증 가능합니다.")
-        
-
-        return user_exist, do_i_play_list, now
