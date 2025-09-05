5699a2e9717782ac46a80c874bfa9d85c6d4ed05
john8707 <doobgyul@gmail.com>
2025-09-05 17:13:50 +0900

    FEAT (temp) login, signup Integration
    로그인, 회원가입 페이지 연동

--- banhyang/core/forms.py
+++ banhyang/core/forms.py
@@ -1,2 +1,73 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'id': "login_username"}),
+                               label="아이디")
+    password = forms.CharField(widget=forms.PasswordInput(),
+                               label="비밀번호")
+    
+    def clean(self):
+        form_data = self.cleaned_data
+        user_model = get_user_model()
+        try:
+            user = user_model.objects.get(username=form_data.get('username'))
+            check_password = user.check_password(form_data.get('password'))
+            is_confirmed = user.is_confirmed
+            if not check_password:
+                self.add_error("password", ValidationError("비밀번호가 일치하지 않습니다."))
+            
+            elif not is_confirmed:
+                self.add_error(None, ValidationError("승인되지 않은 사용자입니다. 관리자에게 문의하세요."))
+
+        except user_model.DoesNotExist:
+            self.add_error("username", ValidationError("아이디가 존재하지 않습니다."))
+
+
+class SignupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = "아이디"
+        self.fields['username'].widget.attrs.update({'placeholder': "영/숫자 4~12자리",
+                                                     'id' : "signup_username"})
+
+        self.fields['password1'].label = "비밀번호"
+        self.fields['password1'].help_text = None
+        self.fields['password1'].widget.attrs.update({'placeholder': "8자리 이상"})
+
+        self.fields['password2'].label = "비밀번호 확인"
+        self.fields['password2'].help_text = None
+        self.fields['password2'].widget.attrs.update({'placeholder': "8자리 이상"})
+
+        self.fields['name'].label = "이름"
+        self.fields['name'].widget.attrs.update({'placeholder': "김반향"})
+
+        self.fields['email'].label = "이메일"
+        self.fields['email'].widget.attrs.update({'placeholder': "banhyang@yonsei.ac.kr"})
+
+        self.fields['student_id'].label = "학번"
+        self.fields['student_id'].widget.attrs.update({'placeholder': "2018000000"})
+
+        self.fields['phone_number'].label = "전화번호"
+        self.fields['phone_number'].widget.attrs.update({'placeholder': "01012345678"})
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class' : 'signup_form'
+            })
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data['phone_number']
+        if "-" in phone_number:
+            phone_number = phone_number.replace("-","")
+        return phone_number
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password1', 'password2','name', 'email', 'student_id', 'phone_number')
+        help_texts = {
+            'username' : None
+        }
--- banhyang/core/views.py
+++ banhyang/core/views.py
@@ -1,20 +1,64 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
-from django.db.models import Sum
-# Create your views here.
+# 표준 라이브러리
+import typing
 
-#통상적으로 HTML 문서와 같은 이름을 사용하자!!
+# Django Core
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+
+# Project Apps
+from .forms import LoginForm, SignupForm
+
+
+# type alias when response is redirect or response
+RedirectOrResponse = typing.Union[HttpResponse, HttpResponseRedirect]
+
 def index(request):
     return render(request, 'index.html')
 
 def new_index(request) -> HttpResponse:
     return render(request, 'new_index.html')
 
-def new_login(request) -> HttpResponse:
-    return render(request, 'new_login.html')
+def new_login(request:HttpRequest) -> RedirectOrResponse:
+    """
+    로그인 페이지
+    """
+    # 로그인 되어있는 경우 불참 신청 페이지로 redirect
+    if request.user.is_authenticated:
+        return redirect('new_apply')
+
+    form = LoginForm()
+    # 로그인 하는 경우
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=raw_password)
+            if user:
+                auth_login(request, user)
+                return redirect('new_apply')
+
+    return render(request, 'new_login_signup.html', {'form' : form, 'title' : "LOGIN"})
 
 
-def new_signup(request) -> HttpResponse:
-    return render(request, 'new_signup.html')
+def new_signup(request:HttpRequest) -> RedirectOrResponse:
+    """
+    회원가입 페이지
+    """
+    # 로그인 되어있는 경우 불참 신청 페이지로 redirect
+    if request.user.is_authenticated:
+        return redirect('new_apply')
+    
+    form = SignupForm()
+
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "회원가입 신청이 완료되었습니다. 임원진 승인 후 이용 가능합니다.")
+            return redirect('new_index')
+
+    return render(request, 'new_login_signup.html', {'form' : form, 'title' : "SIGN UP"})
